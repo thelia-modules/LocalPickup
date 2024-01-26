8c2ba74685e2067c2d60144949236ff0041496f3
anonymze <anodevfr@gmail.com>
2024-01-26 16:21:52 +0100

    add: wysiwyg

--- I18n/en_US.php
+++ I18n/en_US.php
@@ -1,6 +1,7 @@
 <?php
 
 return array(
+    'Commentary local pickup' => 'Write a comment visible for the user when selecting this delivery mode',
     'Price' => 'Price',
     'price must be a number !' => 'price must be a number !',
 );
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,6 +1,7 @@
 <?php
 
 return array(
+    'Commentary local pickup' => 'Ecrivez un commentaire visible pour l\'utilisateur lors de la sélection de ce mode de livraison',
     'Price' => 'Prix',
     'price must be a number !' => 'Le prix doit être un nombre !',
 );

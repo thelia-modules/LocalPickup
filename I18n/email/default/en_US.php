1d8377255cbdb9e4682fba955ebd15a2fe7a8745
anonymze <anodevfr@gmail.com>
2024-06-05 14:26:35 +0200

    feature: template email + event send

---
+++ I18n/email/default/en_US.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'Instructions'=> 'Instructions',
+    'Your order is available in store' => 'Your order is available in store',
+    'Come pick up your order at the store' => 'Your order has been processed. You can collect it during store opening hours.',
+    'Delivery address' => 'Delivery address',
+    'Thanks' => 'Thanks',
+    'The %store team.' => 'The %store team.',
+);

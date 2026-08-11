5589e769c9ab2d72dabb615edef8a6fbf73d2481
Nicolas Barbey <nbarbey@openstudio.fr>
2026-08-11 14:33:50 +0200

    fix: add translation for save buttons

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -3,4 +3,6 @@
 return array(
     'Configure local pickup' => 'Configurer le retrait sur place',
     'For SMS to work, please see <a target="_new" href="%url">%url</a> for required additional configuration' => 'Pour les étapes de configuration du système d\'envoi des SMS, voir <a target="_new" href="%url">%url</a>.',
+    'Save changes' => 'Enregistrer les modifications',
+    'Close' => 'Fermer',
 );
